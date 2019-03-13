41edf223f5a1878c3eabc72b5361fd8fcba85768
Thomas <at93om@gmail.com>
2019-03-12 21:36:39 -0400

    Express Server Set

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+app.use(express.static('app'));
+
+app.listen(PORT, ()=>{
+    console.log('Server listenning to http://localhost:'+PORT);
+})
