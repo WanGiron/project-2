fb473ba67940af27421fbe06c85aea09ea12304f
William and Lady Navas <ladysantoyo@Williams-MacBook-Pro.local>
2019-03-17 18:12:53 -0400

    new-routes-added

--- app/public/java_script/main.js
+++ app/public/java_script/main.js
@@ -4,14 +4,13 @@ $(document).ready(function () {
 
     //script for client site //
     $("#submit-child-request").on("click", (event) => {
+        event.preventDefault();
         var myChild = $("#pull-child").val();
         $.get("/api/client-site/:" + myChild)
             .then(
                 window.location = "/api/client-site/" + myChild
             )
-
     })
-    //-----------------------//
 
 
     $("#get-one").on("click", function (event) {
@@ -41,13 +40,13 @@ $(document).ready(function () {
 
     $("#get-activity").on("click", function (event) {
         event.preventDefault();
-        var child2 = $("#add-child-name2");
-        var activity = $("#add-activity");
+        var activity = $("#add-activity").val();
+        var child2 = $("h3").text();
 
         // Make a newBook object
         var newActivity = {
-            child_name: child2.val(),
-            activity_type: activity.val()
+            child_name: child2,
+            activity_type: activity
         };
 
         // Send an AJAX POST-request with jQuery
@@ -61,6 +60,56 @@ $(document).ready(function () {
         // Empty each input box by replacing the value with an empty string
         child2.val("");
         activity.val("");
+
+    });
+    //creating activities from index adiministrator site // 
+    $("#submit-activity").on("click", function (event) {
+        event.preventDefault();
+        var activity = $("#add-activity").val();
+        var child2 = $("#child-name-activity").val();
+
+        // Make a newBook object
+        var newActivity = {
+            child_name: child2,
+            activity_type: activity
+        };
+
+        // Send an AJAX POST-request with jQuery
+        $.post("/api/new/activity", newActivity)
+            // On success, run the following code
+            .then(function (data) {
+                // Log the data we found
+                console.log(data);
+            });
+
+        // Empty each input box by replacing the value with an empty string
+        child2.val("");
+        activity.val("");
+
+    });
+    // TO send message from parent //
+    $("#send-message").on("click", function (event) {
+        event.preventDefault();
+        var child3 = $("h1").text();
+        var message = $("#new-message").val();
+
+        // Make a newBook object
+        var newMessage = {
+            child_name: child3,
+            message_from_parent: message
+        };
+
+        // Send an AJAX POST-request with jQuery
+        $.post("/api/new/message", newMessage)
+            // On success, run the following code
+            .then(function (data) {
+                // Log the data we found
+                console.log(data);
+            });
+
+        // Empty each input box by replacing the value with an empty string
+        child3.val("");
+        message.val("");
 
     });
 });
--- app/routes/api_routes.js
+++ app/routes/api_routes.js
@@ -1,30 +1,63 @@
 var daycare_data = require("../data_base/daycare_data.js");
+var message_data = require("../data_base/message_data.js")
 var express = require("express");
 //var router = express.Router();
 
 module.exports = (app) => {
     //Administrator site //
-    app.get("/api/adiministrator", (req, res) => {
+    app.get("/api/administrator", (req, res) => {
         daycare_data.children.findAll({}).then((results) => {
             //var newData = parse(results);
             res.render("index", {
                 data: results
             })
-            //console.log(results);
         });
     });
-    // Reqding All children in DB
-    app.get("/api/all-activities", (req, res) => {
-        daycare_data.activities.findAll({}).then((results) => {
-            //var newData = parse(results);
+
+
+// view messages from admin site / kids //
+    app.get("/api/adiministrator/messages", (req, res) => {
+        message_data.message.findAll({}).then((results) => {
             res.render("index", {
-                data: results
+                messageData: results
             })
-            //console.log(results);
         });
     });
-    //Adding a New Child Into DB for administrator site //
-    app.post("/api/new/child", (req, res) => {
+
+    // Findig one child for admin site for activities//
+    app.get("/api/admin-activity-site/:childName", (req, res) => {
+        daycare_data.activities.findAll({
+            where:{
+                child_name: req.params.childName
+            }
+        }).then((results) => {
+            var childName = results[0].dataValues.child_name;
+            res.render("adminAct", {
+                data: results,
+                data2: childName
+            })
+        });
+    });
+
+
+    //Pulling All messages from admin site kids tab based on name//
+    app.get("/api/admin-message-site/:childName", (req, res) => {
+        //var name = req.params.name;
+        message_data.message.findAll({
+            where: {
+                child_name: req.params.childName
+              }
+        }).then((results) => {
+            var childName = results[0].child_name;
+            res.render("messages", {
+                data3: results,
+                dataName: childName
+            })
+        });
+    });
+
+     //Adding a New Child Into DB for administrator site //
+     app.post("/api/new/child", (req, res) => {
         console.log("daycare data:");
         //console.log(req.body);
         daycare_data.children.create({
@@ -35,6 +68,18 @@ module.exports = (app) => {
         });
     });
 
+    // Reqding All children in DB
+    // app.get("/api/all-activities", (req, res) => {
+    //     daycare_data.activities.findAll({}).then((results) => {
+    //         //var newData = parse(results);
+    //         res.render("index", {
+    //             data: results
+    //         })
+    //         //console.log(results);
+    //     });
+    // });
+   
+
     //Adding a New Activities Into DB for administrator site
     app.post("/api/new/activity", (req, res) => {
         console.log("daycare data:");
@@ -43,38 +88,53 @@ module.exports = (app) => {
         daycare_data.activities.create({
         child_name: req.body.child_name,
         activity_type: req.body.activity_type,
-
-        // activity_time: req.body.activity_time        
+       
         }).then((results)=>{
             res.end();
         });
     });
+           //  This is for client site // 
+    // //Pulling All activities based on child name //
+    // app.get("/api/activity/:name", (req, res) => {
+    //     var name = req.params.name;
+    //     daycare_data.activities.findAll({
+    //         where: {
+    //             child_name: name
+    //           }
+    //     }).then((results) => {
+    //         res.json(results);
+    //     });
+    // });
 
-    //Pulling All activities based on child name
-    app.get("/api/activity/:name", (req, res) => {
-        var name = req.params.name;
-        daycare_data.activities.findAll({
-            where: {
-                child_name: name
-              }
-        }).then((results) => {
-            res.json(results);
-        });
-    });
 
-    
-// Findig one child for client site //
+
+
+    // Findig one child for clients site for activities//
     app.get("/api/client-site/:childName", (req, res) => {
         daycare_data.activities.findAll({
             where:{
                 child_name: req.params.childName
             }
         }).then((results) => {
-            //var newData = parse(results);
+            var childName = results[0].dataValues.child_name;
             res.render("client", {
-                data: results
+                data: results,
+                data2: childName
             })
-            //console.log(results);
+        });
+    });
+
+    //Adding a new message Into DB for client site//
+    app.post("/api/new/message", (req, res) => {
+        console.log("message-data");
+        console.log(req.body);
+
+        message_data.message.create({
+        child_name: req.body.child_name,
+        message_from_parent: req.body.message_from_parent,
+  
+        }).then((results)=>{
+            res.end();
         });
     });
 }
