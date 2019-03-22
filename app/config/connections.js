da5e0315e49f11a1a5658bce16feb4f87a8e599b
unknown <rnakada90@gmail.com>
2019-03-21 21:57:40 -0400

    trying to set sequalize with heroku

--- app/config/connections.js
+++ app/config/connections.js
@@ -1,33 +1,39 @@
-const Sequelize = require("sequelize");
-const port = process.env.PORT || 3306;
+var Sequelize = require("sequelize");
+var port = process.env.PORT || 3306;
+const config = require(__dirname + "/../config/config.json")[env];
 
-const mysql = require("mysql");
-
-if (process.env.JAWSDB_URL){
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+if (config.use_env_variable) {
+    var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
-    connection = mysql.createConnection({
-        host: "localhost",
-        port: 3306,
-        user: "root",
-        password: "root",
-        database: "daycare_db"
-    })
-};
+    var sequelize = new Sequelize("daycare_db", "root", "", {
+        host:"localhost",
+        PORT: port,
+        dialect: "mysql",
+        pool: {
+            max: 5,
+            min: 0,
+            idle: 10000
+        },
+        define: {
+            timestamps: false
+        }
+    });
+}
 
 
-var sequelize = new Sequelize("daycare_db", "root", "root", {
-    host:"localhost",
-    PORT: port,
-    dialect: "mysql",
-    pool: {
-        max: 5,
-        min: 0,
-        idle: 10000
-    },
-    define: {
-        timestamps: false
-    }
-});
+
+// var sequelize = new Sequelize("daycare_db", "root", "", {
+//     host:"localhost",
+//     PORT: port,
+//     dialect: "mysql",
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         idle: 10000
+//     },
+//     define: {
+//         timestamps: false
+//     }
+// });
 
 module.exports = sequelize;
