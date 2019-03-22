e73d60c0ec5f0f75ff239de86e85f4b3980a8266
unknown <rnakada90@gmail.com>
2019-03-21 21:33:18 -0400

    jawsDB

--- app/config/connections.js
+++ app/config/connections.js
@@ -1,7 +1,22 @@
-var Sequelize = require("sequelize");
-var port = process.env.PORT || 3306;
+const Sequelize = require("sequelize");
+const port = process.env.PORT || 3306;
 
-var sequelize = new Sequelize("daycare_db", "root", "", {
+const mysql = require("mysql");
+
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "root",
+        database: "daycare_db"
+    })
+};
+
+
+var sequelize = new Sequelize("daycare_db", "root", "root", {
     host:"localhost",
     PORT: port,
     dialect: "mysql",
