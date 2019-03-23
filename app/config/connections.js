b1ded6fd7bd6ada5fe00adca1f40eff98ca99fb6
unknown <rnakada90@gmail.com>
2019-03-22 21:15:28 -0400

    jawdb

--- app/config/connections.js
+++ app/config/connections.js
@@ -1,8 +1,9 @@
+
 var Sequelize = require("sequelize");
 var port = process.env.PORT || 3306;
 
-var sequelize = new Sequelize("daycare_db", "root", "", {
-    host:"localhost",
+var sequelize = new Sequelize("voni79jxs6rfvaez", "vga9ankjkkwe1ixj", "qjk4w9qnz43wi0n4", {
+    host:"v02yrnuhptcod7dk.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     PORT: port,
     dialect: "mysql",
     pool: {
