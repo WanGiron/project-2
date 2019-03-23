91c5b1266f044ec4c06dfcfafee4101ec7d20017
William and Lady Navas <ladysantoyo@Williams-MacBook-Pro.local>
2019-03-23 12:48:14 -0400

    styling

--- app/config/connections.js
+++ app/config/connections.js
@@ -1,5 +1,21 @@
 var Sequelize = require("sequelize");
 var port = process.env.PORT || 3306;
+// var env = process.env.NODE_ENV || "development";
+// var config = require(__dirname + "/config.json")[env];
+
+// var sequelize = new Sequelize(process.env[config.use_env_variable],{
+//     host:"otwsl2e23jrxcqvx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+//     PORT: port,
+//     dialect: "mysql",
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         idle: 10000
+//     },
+//     define: {
+//         timestamps: false
+//     }
+//  });
 
 var sequelize = new Sequelize("y8h3lffj9ny2umdf", "rebb4wiu9t4460yi", "wedq1n8maiorl5q6", {
     host:"otwsl2e23jrxcqvx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
