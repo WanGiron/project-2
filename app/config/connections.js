e1637841c4da83f76957ee01ecdabf43d51a7f1f
William and Lady Navas <ladysantoyo@Williams-MacBook-Pro.local>
2019-03-22 23:37:37 -0400

    heroku-master

--- app/config/connections.js
+++ app/config/connections.js
@@ -1,8 +1,8 @@
 var Sequelize = require("sequelize");
 var port = process.env.PORT || 3306;
 
-var sequelize = new Sequelize("y8h3lffj9ny2umdf", "rebb4wiu9t4460yi", "bg56mpksdpa6qqnb", {
-    host:"otwsl2e23jrxcqvx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
+var sequelize = new Sequelize("y8h3lffj9ny2umdf", "rebb4wiu9t4460yi", "wedq1n8maiorl5q6", {
+    host:"otwsl2e23jrxcqvx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     PORT: port,
     dialect: "mysql",
     pool: {
