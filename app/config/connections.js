185628a0ea1ae12006f4b1dc2196bc1f909c17f7
unknown <rnakada90@gmail.com>
2019-03-22 21:29:33 -0400

    total update

--- app/config/connections.js
+++ app/config/connections.js
@@ -1,8 +1,8 @@
 var Sequelize = require("sequelize");
 var port = process.env.PORT || 3306;
 
-var sequelize = new Sequelize("daycare_db", "root", "", {
-    host:"localhost",
+var sequelize = new Sequelize("orolog6w9j1s72l4", "omcyw45bmqhwjxk6", "c7bj0r67uqrvug9c", {
+    host:"v02yrnuhptcod7dk.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     PORT: port,
     dialect: "mysql",
     pool: {
@@ -14,5 +14,7 @@ var sequelize = new Sequelize("daycare_db", "root", "", {
         timestamps: false
     }
 });
+
+
 
 module.exports = sequelize;
